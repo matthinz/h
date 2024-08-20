98cc5e2b5db2b98220f5dff9b1590449719aa3be
Matt Hinz <matthinz@gmail.com>
2024-08-20 09:52:54 -0700

    DocType build

---
+++ dist/doctype.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HTML = void 0;
+exports.HTML = {
+    __content__: "<!doctype html>",
+};
+//# sourceMappingURL=doctype.js.map
