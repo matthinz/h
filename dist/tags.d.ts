35c1d2005e5d00026c5e04b8323cdd99dcd39da2
Matt Hinz <matthinz@gmail.com>
2024-08-23 16:27:56 -0700

    Absolutely 100% of all html tags

--- dist/tags.d.ts
+++ dist/tags.d.ts
@@ -1,5 +1,5 @@
 import { TaggedH } from "./types";
-export declare const HTML_TAGS: readonly ["a", "body", "address", "article", "div", "h1", "h2", "h3", "h4", "h5", "h6", "li", "ol", "p", "section", "span", "ul"];
+export declare const HTML_TAGS: readonly ["a", "body", "address", "article", "div", "h1", "h2", "h3", "h4", "h5", "h6", "head", "html", "li", "link", "meta", "ol", "p", "section", "span", "ul"];
 export declare const UNCLOSED_TAGS: {
     [key: string]: boolean | undefined;
 };
